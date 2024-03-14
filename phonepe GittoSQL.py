7fcbeacc7c7607f5ca613aabc958a91b8a5af0db
GDharan10 <161794250+GDharan10@users.noreply.github.com>
2024-03-14 21:38:25 +0530

    Add files via upload

--- phonepe GittoSQL.py
+++ phonepe GittoSQL.py
@@ -7,230 +7,227 @@ from sqlalchemy import create_engine
 engine = create_engine('postgresql+psycopg2://postgres:1005@localhost/phonepay')
 
 #1 aggregated/transaction
-path="C:/GD/Notes/DS Class/DTM15/Project/Phonepay project/github/pulse/data/aggregated/transaction/country/india/state/"
-Agg_state_list=os.listdir(path)
-Agg_state_list
+path="C:/GD/Notes/DS Class/DTM15/Project/Phonepe project/github/pulse/data/aggregated/transaction/country/india/state/"
+state_list=os.listdir(path)
 
-clm={'State':[], 'Year':[],'Quater':[],'Transacion_type':[], 'Transacion_count':[], 'Transacion_amount':[]}
+AT_clm={'States':[], 'Years':[],'Quater':[],'Transaction_type':[], 'Transaction_count':[], 'Transaction_amount':[]}
 
-for i in Agg_state_list:
-    p_i=path+i+"/"
-    Agg_yr=os.listdir(p_i)
-    for j in Agg_yr:
-        p_j=p_i+j+"/"
-        Agg_yr_list=os.listdir(p_j)
-        for k in Agg_yr_list:
-            p_k=p_j+k
-            Data=open(p_k,'r')
+for state in state_list:
+    p_s=path+state+"/"
+    years_list=os.listdir(p_s)
+    for year in years_list:
+        p_y=p_s+year+"/"
+        quater_list=os.listdir(p_y)
+        for quater in quater_list:
+            p_q=p_y+quater
+            Data=open(p_q,'r')
             D=json.load(Data)
-            for z in D['data']['transactionData']:
-              Name=z['name']
-              count=z['paymentInstruments'][0]['count']
-              amount=z['paymentInstruments'][0]['amount']
-              clm['Transacion_type'].append(Name)
-              clm['Transacion_count'].append(count)
-              clm['Transacion_amount'].append(amount)
-              clm['State'].append(i)
-              clm['Year'].append(j)
-              clm['Quater'].append(int(k.strip('.json')))
+            for i in D['data']['transactionData']:
+              Name=i['name']
+              count=i['paymentInstruments'][0]['count']
+              amount=i['paymentInstruments'][0]['amount']
+              AT_clm['Transaction_type'].append(Name)
+              AT_clm['Transaction_count'].append(count)
+              AT_clm['Transaction_amount'].append(amount)
+              AT_clm['States'].append(state)
+              AT_clm['Years'].append(year)
+              AT_clm['Quater'].append(int(quater.strip('.json')))
 
-agg_trans=pd.DataFrame(clm)
+agg_trans=pd.DataFrame(AT_clm)
 
-agg_trans['State']=agg_trans['State'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
-agg_trans['State']=agg_trans['State'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
-agg_trans['State']=agg_trans['State'].str.replace('dadra-&-nagar-haveli-&-daman-&-diu','dadra and nagar haveli and daman and diu')
-agg_trans['State']=agg_trans['State'].str.replace("-"," ")
-agg_trans['State']=agg_trans['State'].str.title()
+agg_trans['States']=agg_trans['States'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
+agg_trans['States']=agg_trans['States'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
+agg_trans['States']=agg_trans['States'].str.replace('dadra-&-nagar-haveli-&-daman-&-diu','dadra and nagar haveli and daman and diu')
+agg_trans['States']=agg_trans['States'].str.replace("-"," ")
+agg_trans['States']=agg_trans['States'].str.title()
+
 
 
 #2 aggregated/user
-path="C:/GD/Notes/DS Class/DTM15/Project/Phonepay project/github/pulse/data/aggregated/user/country/india/state/"
-Agg_state_list=os.listdir(path)
-Agg_state_list
+path="C:/GD/Notes/DS Class/DTM15/Project/Phonepe project/github/pulse/data/aggregated/user/country/india/state/"
+state_list=os.listdir(path)
 
-clm={'State':[], 'Year':[],'Quater':[],'user_brand':[], 'reg_userbrand_count':[], 'reg_percentage':[]}
+AU_clm={'States':[], 'Years':[],'Quater':[],'Brands':[], 'Reg_user_count':[], 'Reg_percentage':[]}
 
-for i in Agg_state_list:
-    p_i=path+i+"/"
-    Agg_yr=os.listdir(p_i)
-    for j in Agg_yr:
-        p_j=p_i+j+"/"
-        Agg_yr_list=os.listdir(p_j)
-        for k in Agg_yr_list:
-            p_k=p_j+k
-            Data=open(p_k,'r')
+for state in state_list:
+    p_s=path+state+"/"
+    years_list=os.listdir(p_s)
+    for year in years_list:
+        p_y=p_s+year+"/"
+        quater_list=os.listdir(p_y)
+        for quater in quater_list:
+            p_q=p_y+quater
+            Data=open(p_q,'r')
             D=json.load(Data)
             try:
-                for z in D['data']['usersByDevice']:
-                    Brand=z['brand']
-                    count=z['count']
-                    percentage=z['percentage']
-                    clm['user_brand'].append(Brand)
-                    clm['reg_userbrand_count'].append(count)
-                    clm['reg_percentage'].append(percentage)
-                    clm['State'].append(i)
-                    clm['Year'].append(j)
-                    clm['Quater'].append(int(k.strip('.json')))
+                for i in D['data']['usersByDevice']:
+                    Brand=i['brand']
+                    count=i['count']
+                    percentage=i['percentage']
+                    AU_clm['Brands'].append(Brand)
+                    AU_clm['Reg_user_count'].append(count)
+                    AU_clm['Reg_percentage'].append(percentage)
+                    AU_clm['States'].append(state)
+                    AU_clm['Years'].append(year)
+                    AU_clm['Quater'].append(int(quater.strip('.json')))
             except:
                 pass
 
-agg_users=pd.DataFrame(clm)
+agg_users=pd.DataFrame(AU_clm)
 
-agg_users['State']=agg_users['State'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
-agg_users['State']=agg_users['State'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
-agg_users['State']=agg_users['State'].str.replace('dadra-&-nagar-haveli-&-daman-&-diu','dadra and nagar haveli and daman and diu')
-agg_users['State']=agg_users['State'].str.replace("-"," ")
-agg_users['State']=agg_users['State'].str.title()
+agg_users['States']=agg_users['States'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
+agg_users['States']=agg_users['States'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
+agg_users['States']=agg_users['States'].str.replace('dadra-&-nagar-haveli-&-daman-&-diu','dadra and nagar haveli and daman and diu')
+agg_users['States']=agg_users['States'].str.replace("-"," ")
+agg_users['States']=agg_users['States'].str.title()
 
 
 #3 map/transaction
-path="C:/GD/Notes/DS Class/DTM15/Project/Phonepay project/github/pulse/data/map/transaction/hover/country/india/state/"
-map_state_list=os.listdir(path)
-map_state_list
+path="C:/GD/Notes/DS Class/DTM15/Project/Phonepe project/github/pulse/data/map/transaction/hover/country/india/state/"
+state_list=os.listdir(path)
 
-clm={'State':[], 'Year':[],'Quater':[],'district_name':[], 'transaction_count':[], 'transaction_amount':[]}
 
-for i in map_state_list:
-    p_i=path+i+"/"
-    map_yr=os.listdir(p_i)
-    for j in map_yr:
-        p_j=p_i+j+"/"
-        map_yr_list=os.listdir(p_j)
-        for k in map_yr_list:
-            p_k=p_j+k
-            Data=open(p_k,'r')
+MT_clm={'States':[], 'Years':[],'Quater':[],'District_name':[], 'Transaction_count':[], 'Transaction_amount':[]}
+
+for state in state_list:
+    p_s=path+state+"/"
+    years_list=os.listdir(p_s)
+    for year in years_list:
+        p_y=p_s+year+"/"
+        quater_list=os.listdir(p_y)
+        for quater in quater_list:
+            p_q=p_y+quater
+            Data=open(p_q,'r')
             D=json.load(Data)
             try:
-              for z in D['data']['hoverDataList']:
-                Name=z['name']
-                count=z['metric'][0]['count']
-                amount=z['metric'][0]['amount']
-                clm['district_name'].append(Name)
-                clm['transaction_count'].append(count)
-                clm['transaction_amount'].append(amount)
-                clm['State'].append(i)
-                clm['Year'].append(j)
-                clm['Quater'].append(int(k.strip('.json')))
+              for i in D['data']['hoverDataList']:
+                Name=i['name']
+                count=i['metric'][0]['count']
+                amount=i['metric'][0]['amount']
+                MT_clm['District_name'].append(Name)
+                MT_clm['Transaction_count'].append(count)
+                MT_clm['Transaction_amount'].append(amount)
+                MT_clm['States'].append(state)
+                MT_clm['Years'].append(year)
+                MT_clm['Quater'].append(int(quater.strip('.json')))
             except Exception as e:
-              print(f"Error processing file '{p_k}': {e}")
+              print(f"Error processing file '{p_q}': {e}")
 
-map_trans=pd.DataFrame(clm)
+map_trans=pd.DataFrame(MT_clm)
 
-map_trans['State']=map_trans['State'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
-map_trans['State']=map_trans['State'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
-map_trans['State']=map_trans['State'].str.replace('dadra-&-nagar-haveli-&-daman-&-diu','dadra and nagar haveli and daman and diu')
-map_trans['State']=map_trans['State'].str.replace("-"," ")
-map_trans['State']=map_trans['State'].str.title()
+map_trans['States']=map_trans['States'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
+map_trans['States']=map_trans['States'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
+map_trans['States']=map_trans['States'].str.replace('dadra-&-nagar-haveli-&-daman-&-diu','dadra and nagar haveli and daman and diu')
+map_trans['States']=map_trans['States'].str.replace("-"," ")
+map_trans['States']=map_trans['States'].str.title()
 
 
 #4 map/User
-path="C:/GD/Notes/DS Class/DTM15/Project/Phonepay project/github/pulse/data/map/user/hover/country/india/state/"
-map_state_list=os.listdir(path)
-map_state_list
+path="C:/GD/Notes/DS Class/DTM15/Project/Phonepe project/github/pulse/data/map/user/hover/country/india/state/"
+state_list=os.listdir(path)
 
-clm={'State':[], 'Year':[],'Quater':[],'District_name':[], 'reg_users':[], 'app_opens':[]}
+MU_clm={'States':[], 'Years':[],'Quater':[],'District_name':[], 'Reg_users':[], 'App_opens':[]}
 
-for i in map_state_list:
-    p_i=path+i+"/"
-    map_yr=os.listdir(p_i)
-    for j in map_yr:
-        p_j=p_i+j+"/"
-        map_yr_list=os.listdir(p_j)
-        for k in map_yr_list:
-            p_k=p_j+k
-            Data=open(p_k,'r')
+for state in state_list:
+    p_s=path+state+"/"
+    years_list=os.listdir(p_s)
+    for year in years_list:
+        p_y=p_s+year+"/"
+        quater_list=os.listdir(p_y)
+        for quater in quater_list:
+            p_q=p_y+quater
+            Data=open(p_q,'r')
             D=json.load(Data)
-            for z in D['data']['hoverData'].items():
-              district=z[0]
-              regusers=z[1]['registeredUsers']
-              appopens=z[1]['appOpens']
-              clm['District_name'].append(district)
-              clm['reg_users'].append(regusers)
-              clm['app_opens'].append(appopens)
-              clm['State'].append(i)
-              clm['Year'].append(j)
-              clm['Quater'].append(int(k.strip('.json')))
+            for i in D['data']['hoverData'].items():
+              district=i[0]
+              regusers=i[1]['registeredUsers']
+              appopens=i[1]['appOpens']
+              MU_clm['District_name'].append(district)
+              MU_clm['Reg_users'].append(regusers)
+              MU_clm['App_opens'].append(appopens)
+              MU_clm['States'].append(state)
+              MU_clm['Years'].append(year)
+              MU_clm['Quater'].append(int(quater.strip('.json')))
 
-map_users=pd.DataFrame(clm)
+map_users=pd.DataFrame(MU_clm)
 
-map_users['State']=map_users['State'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
-map_users['State']=map_users['State'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
-map_users['State']=map_users['State'].str.replace('dadra-&-nagar-haveli-&-daman-&-diu','dadra and nagar haveli and daman and diu')
-map_users['State']=map_users['State'].str.replace("-"," ")
-map_users['State']=map_users['State'].str.title()
-
+map_users['States']=map_users['States'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
+map_users['States']=map_users['States'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
+map_users['States']=map_users['States'].str.replace('dadra-&-nagar-haveli-&-daman-&-diu','dadra and nagar haveli and daman and diu')
+map_users['States']=map_users['States'].str.replace("-"," ")
+map_users['States']=map_users['States'].str.title()
 
 #5 Top transaction
-path="C:/GD/Notes/DS Class/DTM15/Project/Phonepay project/github/pulse/data/top/transaction/country/india/state/"
-top_state_list=os.listdir(path)
-top_state_list
+path="C:/GD/Notes/DS Class/DTM15/Project/Phonepe project/github/pulse/data/top/transaction/country/india/state/"
+state_list=os.listdir(path)
 
-clm={'State':[], 'Year':[],'Quater':[],'pincodes':[], 'transaction_count':[], 'transaction_amount':[]}
 
-for i in top_state_list:
-    p_i=path+i+"/"
-    top_yr=os.listdir(p_i)
-    for j in top_yr:
-        p_j=p_i+j+"/"
-        top_yr_list=os.listdir(p_j)
-        for k in top_yr_list:
-            p_k=p_j+k
-            Data=open(p_k,'r')
+TT_clm={'States':[], 'Years':[],'Quater':[],'Pincodes':[], 'Transaction_count':[], 'Transaction_amount':[]}
+
+for state in state_list:
+    p_s=path+state+"/"
+    years_list=os.listdir(p_s)
+    for year in years_list:
+        p_y=p_s+year+"/"
+        quater_list=os.listdir(p_y)
+        for quater in quater_list:
+            p_q=p_y+quater
+            Data=open(p_q,'r')
             D=json.load(Data)
-            for z in D['data']['pincodes']:
-                Entity_name=z['entityName']
-                count=z['metric']['count']
-                amount=z['metric']['amount']
-                clm['pincodes'].append(Entity_name)
-                clm['transaction_count'].append(count)
-                clm['transaction_amount'].append(amount)
-                clm['State'].append(i)
-                clm['Year'].append(j)
-                clm['Quater'].append(int(k.strip('.json')))
+            for i in D['data']['pincodes']:
+                Entity_name=i['entityName']
+                count=i['metric']['count']
+                amount=i['metric']['amount']
+                TT_clm['Pincodes'].append(Entity_name)
+                TT_clm['Transaction_count'].append(count)
+                TT_clm['Transaction_amount'].append(amount)
+                TT_clm['States'].append(state)
+                TT_clm['Years'].append(year)
+                TT_clm['Quater'].append(int(quater.strip('.json')))
 
-top_trans=pd.DataFrame(clm)
+top_trans=pd.DataFrame(TT_clm)
 
-top_trans['State']=top_trans['State'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
-top_trans['State']=top_trans['State'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
-top_trans['State']=top_trans['State'].str.replace('dadra-&-nagar-haveli-&-daman-&-diu','dadra and nagar haveli and daman and diu')
-top_trans['State']=top_trans['State'].str.replace("-"," ")
-top_trans['State']=top_trans['State'].str.title()
+top_trans['States']=top_trans['States'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
+top_trans['States']=top_trans['States'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
+top_trans['States']=top_trans['States'].str.replace('dadra-&-nagar-haveli-&-daman-&-diu','dadra and nagar haveli and daman and diu')
+top_trans['States']=top_trans['States'].str.replace("-"," ")
+top_trans['States']=top_trans['States'].str.title()
 
 
 
 #6 top/user
-path="C:/GD/Notes/DS Class/DTM15/Project/Phonepay project/github/pulse/data/top/user/country/india/state/"
-top_state_list=os.listdir(path)
-top_state_list
+path="C:/GD/Notes/DS Class/DTM15/Project/Phonepe project/github/pulse/data/top/user/country/india/state/"
+state_list=os.listdir(path)
 
-clm={'State':[], 'Year':[],'Quater':[],'Pincodes': [], 'reg_users':[]}
 
-for i in top_state_list:
-    p_i=path+i+"/"
-    top_yr=os.listdir(p_i)
-    for j in top_yr:
-        p_j=p_i+j+"/"
-        top_yr_list=os.listdir(p_j)
-        for k in top_yr_list:
-            p_k=p_j+k
-            Data=open(p_k,'r')
+TU_clm={'States':[], 'Years':[],'Quater':[],'Pincodes': [], 'Reg_users':[]}
+
+for state in state_list:
+    p_s=path+state+"/"
+    years_list=os.listdir(p_s)
+    for year in years_list:
+        p_y=p_s+year+"/"
+        quater_list=os.listdir(p_y)
+        for quater in quater_list:
+            p_q=p_y+quater
+            Data=open(p_q,'r')
             D=json.load(Data)
-            for z in D['data']['pincodes']:
-                Name=z['name']
-                reg_users=z['registeredUsers']
-                clm['Pincodes'].append(Name)
-                clm['reg_users'].append(reg_users)
-                clm['State'].append(i)
-                clm['Year'].append(j)
-                clm['Quater'].append(int(k.strip('.json')))
+            for i in D['data']['pincodes']:
+                Name=i['name']
+                reg_users=i['registeredUsers']
+                TU_clm['Pincodes'].append(Name)
+                TU_clm['Reg_users'].append(reg_users)
+                TU_clm['States'].append(state)
+                TU_clm['Years'].append(year)
+                TU_clm['Quater'].append(int(quater.strip('.json')))
 
-top_user=pd.DataFrame(clm)
+top_user=pd.DataFrame(TU_clm)
 
-top_user['State']=top_user['State'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
-top_user['State']=top_user['State'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
-top_user['State']=top_user['State'].str.replace('dadra-&-nagar-haveli-&-daman-&-diu','dadra and nagar haveli and daman and diu')
-top_user['State']=top_user['State'].str.replace("-"," ")
-top_user['State']=top_user['State'].str.title()
+top_user['States']=top_user['States'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
+top_user['States']=top_user['States'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
+top_user['States']=top_user['States'].str.replace('dadra-&-nagar-haveli-&-daman-&-diu','dadra and nagar haveli and daman and diu')
+top_user['States']=top_user['States'].str.replace("-"," ")
+top_user['States']=top_user['States'].str.title()
 
 
 
