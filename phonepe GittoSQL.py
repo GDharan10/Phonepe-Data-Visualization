869b325e26dd8001bc39f28a1d74d24cd9a008e7
GDharan10 <161794250+GDharan10@users.noreply.github.com>
2024-03-22 07:16:16 +0530

    Add files via upload

--- phonepe GittoSQL.py
+++ phonepe GittoSQL.py
@@ -6,11 +6,12 @@ import psycopg2
 from sqlalchemy import create_engine
 engine = create_engine('postgresql+psycopg2://postgres:1005@localhost/phonepay')
 
+
 #1 aggregated/transaction
 path="C:/GD/Notes/DS Class/DTM15/Project/Phonepe project/github/pulse/data/aggregated/transaction/country/india/state/"
 state_list=os.listdir(path)
 
-AT_clm={'States':[], 'Years':[],'Quater':[],'Transaction_type':[], 'Transaction_count':[], 'Transaction_amount':[]}
+agg_trans_clm={'States':[], 'Years':[],'Quater':[],'Transaction_type':[], 'Transaction_count':[], 'Transaction_amount':[]}
 
 for state in state_list:
     p_s=path+state+"/"
@@ -26,14 +27,14 @@ for state in state_list:
               Name=i['name']
               count=i['paymentInstruments'][0]['count']
               amount=i['paymentInstruments'][0]['amount']
-              AT_clm['Transaction_type'].append(Name)
-              AT_clm['Transaction_count'].append(count)
-              AT_clm['Transaction_amount'].append(amount)
-              AT_clm['States'].append(state)
-              AT_clm['Years'].append(year)
-              AT_clm['Quater'].append(int(quater.strip('.json')))
+              agg_trans_clm['Transaction_type'].append(Name)
+              agg_trans_clm['Transaction_count'].append(count)
+              agg_trans_clm['Transaction_amount'].append(amount)
+              agg_trans_clm['States'].append(state)
+              agg_trans_clm['Years'].append(year)
+              agg_trans_clm['Quater'].append(int(quater.strip('.json')))
 
-agg_trans=pd.DataFrame(AT_clm)
+agg_trans=pd.DataFrame(agg_trans_clm)
 
 agg_trans['States']=agg_trans['States'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
 agg_trans['States']=agg_trans['States'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
@@ -47,7 +48,7 @@ agg_trans['States']=agg_trans['States'].str.title()
 path="C:/GD/Notes/DS Class/DTM15/Project/Phonepe project/github/pulse/data/aggregated/user/country/india/state/"
 state_list=os.listdir(path)
 
-AU_clm={'States':[], 'Years':[],'Quater':[],'Brands':[], 'Reg_user_count':[], 'Reg_percentage':[]}
+agg_user_clm={'States':[], 'Years':[],'Quater':[],'Brands':[], 'Reg_user_count':[], 'Reg_percentage':[]}
 
 for state in state_list:
     p_s=path+state+"/"
@@ -64,16 +65,16 @@ for state in state_list:
                     Brand=i['brand']
                     count=i['count']
                     percentage=i['percentage']
-                    AU_clm['Brands'].append(Brand)
-                    AU_clm['Reg_user_count'].append(count)
-                    AU_clm['Reg_percentage'].append(percentage)
-                    AU_clm['States'].append(state)
-                    AU_clm['Years'].append(year)
-                    AU_clm['Quater'].append(int(quater.strip('.json')))
+                    agg_user_clm['Brands'].append(Brand)
+                    agg_user_clm['Reg_user_count'].append(count)
+                    agg_user_clm['Reg_percentage'].append(percentage)
+                    agg_user_clm['States'].append(state)
+                    agg_user_clm['Years'].append(year)
+                    agg_user_clm['Quater'].append(int(quater.strip('.json')))
             except:
                 pass
 
-agg_users=pd.DataFrame(AU_clm)
+agg_users=pd.DataFrame(agg_user_clm)
 
 agg_users['States']=agg_users['States'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
 agg_users['States']=agg_users['States'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
@@ -87,7 +88,7 @@ path="C:/GD/Notes/DS Class/DTM15/Project/Phonepe project/github/pulse/data/map/t
 state_list=os.listdir(path)
 
 
-MT_clm={'States':[], 'Years':[],'Quater':[],'District_name':[], 'Transaction_count':[], 'Transaction_amount':[]}
+map_trans_clm={'States':[], 'Years':[],'Quater':[],'District_name':[], 'Transaction_count':[], 'Transaction_amount':[]}
 
 for state in state_list:
     p_s=path+state+"/"
@@ -104,16 +105,16 @@ for state in state_list:
                 Name=i['name']
                 count=i['metric'][0]['count']
                 amount=i['metric'][0]['amount']
-                MT_clm['District_name'].append(Name)
-                MT_clm['Transaction_count'].append(count)
-                MT_clm['Transaction_amount'].append(amount)
-                MT_clm['States'].append(state)
-                MT_clm['Years'].append(year)
-                MT_clm['Quater'].append(int(quater.strip('.json')))
+                map_trans_clm['District_name'].append(Name)
+                map_trans_clm['Transaction_count'].append(count)
+                map_trans_clm['Transaction_amount'].append(amount)
+                map_trans_clm['States'].append(state)
+                map_trans_clm['Years'].append(year)
+                map_trans_clm['Quater'].append(int(quater.strip('.json')))
             except Exception as e:
               print(f"Error processing file '{p_q}': {e}")
 
-map_trans=pd.DataFrame(MT_clm)
+map_trans=pd.DataFrame(map_trans_clm)
 
 map_trans['States']=map_trans['States'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
 map_trans['States']=map_trans['States'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
@@ -126,7 +127,7 @@ map_trans['States']=map_trans['States'].str.title()
 path="C:/GD/Notes/DS Class/DTM15/Project/Phonepe project/github/pulse/data/map/user/hover/country/india/state/"
 state_list=os.listdir(path)
 
-MU_clm={'States':[], 'Years':[],'Quater':[],'District_name':[], 'Reg_users':[], 'App_opens':[]}
+map_User_clm={'States':[], 'Years':[],'Quater':[],'District_name':[], 'Reg_users':[], 'App_opens':[]}
 
 for state in state_list:
     p_s=path+state+"/"
@@ -142,14 +143,14 @@ for state in state_list:
               district=i[0]
               regusers=i[1]['registeredUsers']
               appopens=i[1]['appOpens']
-              MU_clm['District_name'].append(district)
-              MU_clm['Reg_users'].append(regusers)
-              MU_clm['App_opens'].append(appopens)
-              MU_clm['States'].append(state)
-              MU_clm['Years'].append(year)
-              MU_clm['Quater'].append(int(quater.strip('.json')))
+              map_User_clm['District_name'].append(district)
+              map_User_clm['Reg_users'].append(regusers)
+              map_User_clm['App_opens'].append(appopens)
+              map_User_clm['States'].append(state)
+              map_User_clm['Years'].append(year)
+              map_User_clm['Quater'].append(int(quater.strip('.json')))
 
-map_users=pd.DataFrame(MU_clm)
+map_users=pd.DataFrame(map_User_clm)
 
 map_users['States']=map_users['States'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
 map_users['States']=map_users['States'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
@@ -157,12 +158,13 @@ map_users['States']=map_users['States'].str.replace('dadra-&-nagar-haveli-&-dama
 map_users['States']=map_users['States'].str.replace("-"," ")
 map_users['States']=map_users['States'].str.title()
 
+
+
 #5 Top transaction
 path="C:/GD/Notes/DS Class/DTM15/Project/Phonepe project/github/pulse/data/top/transaction/country/india/state/"
 state_list=os.listdir(path)
 
-
-TT_clm={'States':[], 'Years':[],'Quater':[],'Pincodes':[], 'Transaction_count':[], 'Transaction_amount':[]}
+top_trans_clm={'States':[], 'Years':[],'Quater':[],'Pincodes':[], 'Transaction_count':[], 'Transaction_amount':[]}
 
 for state in state_list:
     p_s=path+state+"/"
@@ -178,14 +180,14 @@ for state in state_list:
                 Entity_name=i['entityName']
                 count=i['metric']['count']
                 amount=i['metric']['amount']
-                TT_clm['Pincodes'].append(Entity_name)
-                TT_clm['Transaction_count'].append(count)
-                TT_clm['Transaction_amount'].append(amount)
-                TT_clm['States'].append(state)
-                TT_clm['Years'].append(year)
-                TT_clm['Quater'].append(int(quater.strip('.json')))
+                top_trans_clm['Pincodes'].append(Entity_name)
+                top_trans_clm['Transaction_count'].append(count)
+                top_trans_clm['Transaction_amount'].append(amount)
+                top_trans_clm['States'].append(state)
+                top_trans_clm['Years'].append(year)
+                top_trans_clm['Quater'].append(int(quater.strip('.json')))
 
-top_trans=pd.DataFrame(TT_clm)
+top_trans=pd.DataFrame(top_trans_clm)
 
 top_trans['States']=top_trans['States'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
 top_trans['States']=top_trans['States'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
@@ -200,7 +202,7 @@ path="C:/GD/Notes/DS Class/DTM15/Project/Phonepe project/github/pulse/data/top/u
 state_list=os.listdir(path)
 
 
-TU_clm={'States':[], 'Years':[],'Quater':[],'Pincodes': [], 'Reg_users':[]}
+top_user_clm={'States':[], 'Years':[],'Quater':[],'Pincodes': [], 'Reg_users':[]}
 
 for state in state_list:
     p_s=path+state+"/"
@@ -215,13 +217,13 @@ for state in state_list:
             for i in D['data']['pincodes']:
                 Name=i['name']
                 reg_users=i['registeredUsers']
-                TU_clm['Pincodes'].append(Name)
-                TU_clm['Reg_users'].append(reg_users)
-                TU_clm['States'].append(state)
-                TU_clm['Years'].append(year)
-                TU_clm['Quater'].append(int(quater.strip('.json')))
+                top_user_clm['Pincodes'].append(Name)
+                top_user_clm['Reg_users'].append(reg_users)
+                top_user_clm['States'].append(state)
+                top_user_clm['Years'].append(year)
+                top_user_clm['Quater'].append(int(quater.strip('.json')))
 
-top_user=pd.DataFrame(TU_clm)
+top_user=pd.DataFrame(top_user_clm)
 
 top_user['States']=top_user['States'].str.replace('andaman-&-nicobar-islands','andaman & nicobar')
 top_user['States']=top_user['States'].str.replace('jammu-&-kashmir', 'jammu & kashmir')
